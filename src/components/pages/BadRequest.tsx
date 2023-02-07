6a8df25c2da2913db34c80cc64aa5c42ab6b4119
edy_mulya07 <edy.mulyanto07@gmail.com>
2023-02-07 20:05:10 +0700

    feature: ErrorTemplates; Unauthorized; BadRequest

--- src/components/pages/BadRequest.tsx
+++ src/components/pages/BadRequest.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ErrorTemplate from '../templates/ErrorTemplate'
+import React from "react";
+import ErrorTemplate from "../templates/ErrorTemplate";
+import PageNotFoundImage from "../../assets/images/pose5.png";
+import Tree from "../../assets/images/Tree.png";
 
 const BadRequest = () => {
   return (
-    <ErrorTemplate statusCode={404} />
-  )
-}
+    <ErrorTemplate
+      statusCode={404}
+      message="Page Not Found ⚠️"
+      description="we couldn't find the page you are looking for"
+      src={PageNotFoundImage}
+      image={Tree}
+    />
+  );
+};
 
-export default BadRequest
+export default BadRequest;
