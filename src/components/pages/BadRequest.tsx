5a2e6086cdde9ff5385d41145802de5920abc8c4
edy_mulya07 <edy.mulyanto07@gmail.com>
2023-02-22 14:10:37 +0700

    adding feautre/error
    adding templates: ErrorTemplate
    adding pages: Unauthorized & BadRequest
    adding image component

--- src/components/pages/BadRequest.tsx
+++ src/components/pages/BadRequest.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ErrorTemplate from '../templates/ErrorTemplate'
+import React from "react";
+import ErrorTemplate from "../templates/ErrorTemplate";
+import PagNotFoundImage from "../../assets/images/pose5.png";
+import Tree from "../../assets/static/svgs/Tree.svg";
 
 const BadRequest = () => {
   return (
-    <ErrorTemplate statusCode={404} />
-  )
-}
+    <ErrorTemplate
+      statusCode={404}
+      message="Page Not Found ⚠️"
+      description="we couldn't find the page you are looking for"
+      src={PagNotFoundImage}
+      image={Tree}
+    />
+  );
+};
 
-export default BadRequest
+export default BadRequest;
--- src/components/templates/ErrorTemplate.tsx
+++ src/components/templates/ErrorTemplate.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React from "react";
+
+import Button from "../atoms/Button";
 
 type Props = {
-  statusCode: number
-}
+  statusCode: number;
+  message: string;
+  description: string;
+  src: string;
+  image?: string;
+};
 
 const ErrorTemplate = (props: Props) => {
   return (
-    <div>{props.statusCode && props.statusCode} Error bad page Request, edit at <code>src/components/templates/ErrorTemplates</code></div>
-  )
-}
+    <div className="flex flex-col w-full min-h-screen bg-[#F4F5FA]">
+      <div className="flex flex-col h-full items-center justify-between">
+        <h1 className="font-bold text-8xl font-inter text-gray-700">
+          {props.statusCode}
+        </h1>
+        <h2 className="font-normal text-2xl font-inter  text-gray-700 text-center">
+          {props.message}
+        </h2>
+        <p className="font-normal text-sm font-inter mt-4 text-gray-500">
+          {props.description}
+        </p>
+        <img className="my-2" src={props.src} />
+        <div className="mx-[642px] mt-[40px] w-[156px] h-[38px] text-base bg-[#9155FD] text-white py-2 px-2 text-center hover:bg-purple-600 rounded-[8px] active:scale-95">
+          <Button
+            title="BACK TO HOME"
+            action={(e) => {
+              alert("Triggered");
+            }}
+          />
+        </div>
+      </div>
+      <div className="flex absolute ml-[90.07px] mt-[650px]">
+        <img className="w-[88.04px] h-[190px]" src={props.image} />
+      </div>
+    </div>
+  );
+};
 
-export default ErrorTemplate
+export default ErrorTemplate;
