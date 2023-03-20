2cae09a079c0b274df6d1aedcc2294b3a4223005
mahrus-rohisyam <mahrusaim@gmail.com>
2023-03-20 16:57:08 +0700

    fix: Sidebar Functionality

--- src/components/atoms/Logos/Logo.tsx
+++ src/components/atoms/Logos/Logo.tsx
@@ -1,11 +1,12 @@
 import Image from "../Image";
 import mainLogo from "../../../assets/images/mainlogo.svg";
+import { SiteInfo } from "../../../data/SiteInfo";
 
 const Logo = () => {
   return (
     <Image
       src={mainLogo}
-      alt="Dinar Education"
+      alt={SiteInfo.siteTitle}
       width={40}
       height={40}
     />
--- src/components/molecules/MainLogo.tsx
+++ src/components/molecules/MainLogo.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Logo from "../atoms/Logos/Logo";
 import LogoText from "../atoms/Logos/LogoText";
 
-const MainLogo = () => {
+const MainLogo = ({withText}: {withText?: boolean}) => {
   return (
     <div className="flex items-center gap-2">
       <Logo />
-      <LogoText />
+      {withText && <LogoText />}
     </div>
   );
 };
