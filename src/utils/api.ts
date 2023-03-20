6aad143006d2c0a03a939a99b5c45abf2d57d7a2
ari-salt <aristo.rinjuang@salt.co.id>
2023-03-20 23:39:39 +0700

    fix: REACT_APP_CMS_HOST

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: "http://localhost:8080/",
-	method: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+	baseURL: process.env.REACT_APP_CMS_HOST
 });
